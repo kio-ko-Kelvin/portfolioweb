500c93dd7bf6802f3efe904eaa1ee37972cf20a6
kapelo <ano54niomous@gmail.com>
2026-08-02 21:53:32 +0300

    Update database

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -80,11 +80,14 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ["MYSQLDATABASE"],
+        "USER": os.environ["MYSQLUSER"],
+        "PASSWORD": os.environ["MYSQLPASSWORD"],
+        "HOST": os.environ["MYSQLHOST"],
+        "PORT": os.environ["MYSQLPORT"],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
