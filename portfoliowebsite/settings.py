8853d186062d4562b28b8c4c3a12d2dc081e70a9
Anon <ano54niomous@gmail.com>
2026-08-08 20:20:35 +0300

    Finally, the static files are being served correctly

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'portfoliowebsite.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
