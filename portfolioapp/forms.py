3a4334f039ea427977a773bb495b4e1d0bb8c606
Anon <ano54niomous@gmail.com>
2026-08-25 20:49:30 +0300

    Added the ContactMessageForm used for data validation before storage to database

---
+++ portfolioapp/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import ContactMessage
+
+class ContactMessageForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ["fullname", "emailaddress", "mobilenumber", "emailsubject", "message", "created"]
+
+        widgets = {
+            "fullname": forms.TextInput(attrs={
+                "placeholder": "Full name",
+                "class": "input-box",
+            }),
+
+            "amailaddress": forms.EmailInput(attrs={
+                "placeholder" : "Email Address",
+                "class" : "input-box",
+            }),
+
+            "mobilenumber" : forms.NumberInput(attrs={
+                "Placeholder" : "Mobile number",
+                "class" : "input-box",
+            }),
+
+            "emailsubject" : forms.TextInput(attrs={
+                "Placeholder" : "Email Subject",
+                "class" : "input-box",
+            }),
+
+            "message" : forms.Textarea(attrs={
+                "Placeholder" : "Your Message",
+                "class" : "input-box",
+                "rows" : 6,
+            }),
+        }
