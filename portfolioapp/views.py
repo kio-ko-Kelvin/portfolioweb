f6caa10917df12119f2791fa026f889e6b09f9f1
Anon <ano54niomous@gmail.com>
2026-08-25 21:18:31 +0300

    Changed the html form tag to use the values of the form in forms.py

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -24,7 +24,7 @@ def home(request):
                 request,
                 "Your message has been sent successfully!"
             )
-            return redirect(home)
+            return redirect('home')
 
         else:
             messages.error(
